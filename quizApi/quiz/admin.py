082acf9570e31d432a28610eb2faf736454633f5
ridmik <poran.cse@gmail.com>
2021-08-28 22:46:01 +0600

    configured admin.py for all model

--- quizApi/quiz/admin.py
+++ quizApi/quiz/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Category)
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
+
+@admin.register(models.Quizes)
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+    ]
+
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model = models.Answer
+    fields = [
+        'answer_text',
+        'is_right',
+    ]
+
+@admin.register(models.Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'quiz',
+        'technique',
+        'difficulty'
+    ]
+    list_display = [
+        'title',
+        'quiz',
+        'date_updated',
+    ]
+
+    inlines = [
+        AnswerInlineModel,
+    ]
+
+@admin.register(models.Answer)    
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [
+        'answer_text',
+        'is_right',
+        'question'
+    ]
